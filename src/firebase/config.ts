14aafd0ec45516916218f1a49b0979cb2a06e743
Mihael Nas <nasmihael@gmail.com>
2025-11-09 20:08:12 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5852215415-cb248",
+  "appId": "1:1036932691445:web:cce75a1fccb5fd7902dcbc",
+  "apiKey": "AIzaSyA6VoqzVfv64CwcUP97LzfclRoMBX1NcPQ",
+  "authDomain": "studio-5852215415-cb248.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1036932691445"
+};
